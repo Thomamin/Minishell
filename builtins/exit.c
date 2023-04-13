4905ab190192bdfd464d78e8f9b362c8a5c40d03
thomamin <thomamin@hotmail.com>
2023-04-13 16:16:41 +0900

    builtins beta

--- builtins/exit.c
+++ builtins/exit.c
@@ -27,9 +27,11 @@ int mini_exit(t_cmd_info *cmd)
 		}
 		ret = ft_atoi(cmd->cmd_and_av[1]);
 	}	
-	//minishell program exit
-		//exit을 호출한 main process를 종료하도록
 
-	//int argument가 있으면 int로 변환하여 return -> exit_status에 return값 update
-	return (ret);
+	//minishell program exit이 exit() 함수 호출로 exit status code요건을 맞출 수 없을 경우
+		//exit을 호출한 main process를 종료하도록
+		//int argument가 있으면 int로 변환하여 return -> exit_status에 return값 update
+		//return (ret);
+
+	exit(ret);
 }
