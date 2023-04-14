f8092297edb9065183d87a459348de0deaf25b15
thomamin <thomamin@hotmail.com>
2023-04-14 15:41:11 +0900

    cd, env_utils2 modified

--- env/env_utils2.c
+++ env/env_utils2.c
@@ -9,9 +9,14 @@ char	*ft_getenv(t_env_info *env_head, char *key)
 	return (cur->env_val);
 }
 
-void	ft_setenv(t_env_info *env_head, char *key, char *val)
+void	ft_setenv(t_env_info *env_head, char *key_value)
 {
 	t_env_info	*cur;
+	char		*key;
+	char		*val;
+
+	key = get_env_key(key_value);
+	val = get_env_value(key_value);
 
 	cur = compare_env_key(env_head, key);
 	if (!ft_strcmp(cur->env_key, key))  // id key로 기존 변수 찾은 경우
@@ -23,6 +28,7 @@ void	ft_setenv(t_env_info *env_head, char *key, char *val)
 	else //주어진 id로 설정된 변수가 없는 경우
 	{
 		// 새 환경변수 추가
-		;
+		cur->next = new_env(key_value);
+		cur->next->prev = cur;
 	}
 }
